1682e321e0770c72ac9da057df08d2a5c6d0bb88
GokulnathP <4gokul2000@gmail.com>
2024-12-12 17:34:17 +0530

    [Gokul] Add express playground

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import {ApolloServer} from "apollo-server-express";
 import {readFile} from "fs/promises"
 import {resolvers} from "./resolvers.js";
+import expressPlayground from "graphql-playground-middleware-express";
 
 const PORT = 9000
 const GRAPHQL_PATH = "/graphql"
@@ -14,6 +15,7 @@ const apolloServer = new ApolloServer({typeDefs, resolvers})
 await apolloServer.start()
 apolloServer.applyMiddleware({app, path: GRAPHQL_PATH})
 
+app.get('/playground', expressPlayground.default({ endpoint: '/graphql' }))
 app.listen(PORT, () => {
     console.log(`Server endpoint: http://localhost:${PORT}`)
     console.log(`GraphQL endpoint: http://localhost:${PORT}${GRAPHQL_PATH}`)
